daf66c3b3b65366e59d266cab8cf5811b4e47430
hmtmcse <hmtm.cse@gmail.com>
2021-10-29 00:09:27 +0600

    Update various component

--- frame-mod/base-app/src/view/base-app/create-view.tsx
+++ frame-mod/base-app/src/view/base-app/create-view.tsx
@@ -10,6 +10,8 @@ import Button from "@pfo/pf-rui/bootstrap/Button";
 import Toast from "@pfo/pf-rui/bootstrap/Toast";
 import TextField from "@pfo/pf-rui/bootstrap/TextField";
 import LoadingIndicator from "@pfo/pf-rui/bootstrap/LoadingIndicator";
+import {FieldSpecification} from "@pfo/pf-react/src/artifacts/data/pf-input-definition";
+import Select from "@pfo/pf-rui/bootstrap/Select";
 
 
 interface Props extends PFProps {
@@ -17,41 +19,56 @@ interface Props extends PFProps {
 }
 
 class State extends PFComponentState {
-
+    random?: string
 }
 
 
-interface ExampleArguments {
-    scope?: boolean;
-    location?: string;
-    project?: number;
-}
 
 export default class CreateView extends PFComponent<Props, State> {
 
     state: State = new State();
     attributes: { [key: string]: any } = {};
 
-    constructor(props: Props) {
-        super(props);
-    }
 
     componentDidMount() {
+        // const _this = this;
+        // setInterval(() => {
+        //     _this.setState({random: "" + (Math.random() * 100000000000)})
+        //     console.log(_this.state.random)
+        // }, 5000)
     }
 
     componentDidUpdate(prevProps: Props) {
     }
 
-    field(exampleArguments: ExampleArguments) {
-        return exampleArguments
+
+    fieldDefinition(field: FieldSpecification) {
+        field.text({
+            name: "firstName", label: "First Name", changeEvent: {
+                fire(event: any) {
+                }
+            }
+        })
+        field.text({name: "lastName", label: "Last Name"})
+        field.password({name: "password", label: "Password", required: true})
+        field.email({name: "email", label: "Email", required: true})
+        field.select({name: "select", label: "Select", required: true, optionLabel: "label", optionValue: "value", options: []})
     }
 
-    // fieldDefinition() {
-    //     console.log("Create View")
-    // }
+    formSubmit(event: any) {
+        event.preventDefault();
+        const _this = this;
+        try {
+            console.log(this.getFormData())
+        }catch (e: any) {
+            console.log(e.message)
+        }
+
+    }
 
 
     renderUI() {
+        console.log("Updatedddddddd..............................")
         return (
             <React.Fragment>
                 <div className="pt-3 pb-2 mb-2 border-bottom">
@@ -59,17 +76,17 @@ export default class CreateView extends PFComponent<Props, State> {
                 </div>
                 <Card>
                     <Toast messageType={"success"} message={"Bismillah Message"} />
-                    <LoadingIndicator/>
                     <CardContent>
                         <form >
                             <Row>
-                                <TextField name={this.attributes.name} label={"First Name"} required={true} addWrapperClass={"col-6"}/>
-                                <TextField name={"lastName"} label={"Last Name"} addWrapperClass={"col-6"}/>
-                                <TextField name={"email"} label={"Email"} type={"email"} required={true} addWrapperClass={"col-6"}/>
-                                <TextField name={"password"} label={"Password"} type={"password"} required={true} addWrapperClass={"col-6"}/>
+                                <Select {...this.setupFieldAttrs("select")} addWrapperClass={"col-6"}/>
+                                <TextField {...this.setupFieldAttrs("firstName")} addWrapperClass={"col-6"}/>
+                                <TextField {...this.setupFieldAttrs("lastName")} addWrapperClass={"col-6"}/>
+                                <TextField {...this.setupFieldAttrs("email")} addWrapperClass={"col-6"}/>
+                                <TextField {...this.setupFieldAttrs("password")} addWrapperClass={"col-6"}/>
                                 <Column span={12}>
                                     <span className={"float-end"}>
-                                        <Button type={"submit"}>Save</Button>
+                                        <Button type={"submit"} onClick={(event: any) =>{ this.formSubmit(event)}}>Save</Button>
                                         <a href="#" className="btn btn-danger ms-2">Cancel</a>
                                     </span>
                                 </Column>
